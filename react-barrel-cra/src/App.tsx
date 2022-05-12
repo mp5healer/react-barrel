6377e6234c32c795c3dd04890f15934d7d0d5491
photon <photon.khan@texadasoftware.com>
2022-05-13 00:58:49 +0600

    :sparkles: feat: tested out the barrel import in cra environment

---
+++ react-barrel-cra/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { Content, Footer, Header } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-barrel-cra/src/components/Content/index.tsx
@@ -0,0 +1,11 @@
+export const Content = () => {
+    return (
+        <main>
+            <p>
+                Content
+            </p>
+        </main>
+    )
+}
+
+export default Content
